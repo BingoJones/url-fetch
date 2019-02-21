bfce08a4dd26e00901cad71eebcab6bc384e0e11
Ash Ketchum <me@email.com>
2019-02-21 11:36:05 -0800

    POST/GET endpoints work

---
+++ routes/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
+const mongoose = require('mongoose');
+const validUrl = require('valid-url');
+
+mongoose.connect('mongodb://localhost/local', { useNewUrlParser: true });
+
+const { Schema, model } = mongoose;
+
+const JobSchema = new Schema({
+    url: { type: String, required: true },
+    status: { type: String, default: 'pending' },
+    response: { type: String }
+});
+
+const Job = model('Job', JobSchema);
+
+router.post('/jobs', (req, res, next) => {
+    const { url } = req.body;
+
+    if (!validUrl.isUri(url)) {
+	return res.sendStatus(400);
+    }
+
+    const job = new Job({ url });
+
+    job.save((err, jobs) => {
+	if (err) {
+	    console.log(`Unable to create job record for "${url}".  Error: ${err}`);
+	    return res.sendStatus(500);
+	}
+    });
+
+    try {
+	fetch(url)
+	    .then(res => res.text())
+	    .then(body => {
+		Job.findOneAndUpdate({ _id: job.id}, { status: 'done', response: body }).exec();
+	    });
+    } catch (e) {
+	console.log(`Could not fetch contents of "${url}".  Error: ${e}`);
+	return res.sendStatus(500);
+    }
+
+    res.send({ jobId: job.id, status: job.status });
+});
+
+router.get('/jobs/:jobId', (req, res, next) => {
+    const { jobId } = req.params;
+    
+    Job.findById(jobId, (err, job) => {
+	if (err) {
+	    return res.sendStatus(404);
+	}
+
+	const response = {
+	    status: job.status
+	};
+
+	if (job.status === 'done') {
+	    response.html = job.response
+	}
+	
+	res.send(response);
+    });
+});
+
+module.exports = router;
